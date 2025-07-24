601914d2abae728999e75d5ec277d614f5677503
Pawel Baczynski <pawel.baczynski86@gmail.com>
2025-07-24 19:25:48 +0200

    updates

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,4 @@
-# Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -11,8 +10,50 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+def get_request(endpoint, **kwargs):
+    """
+    This function constructs a GET request to a specified backend URL with optional parameters (i.e.:
+    This function calls GET method in requests library with a URL and any URL parameters such as dealerId.)
+
+    Parameters:
+    endpoint (str): The endpoint path to append to the backend URL.
+    **kwargs: Optional keyword arguments representing query parameters. 
+    
+    **kwargs works just like *args, 
+    but instead of accepting positional arguments it accepts keyword (or named) arguments, e.g.:
+
+    def concatenate(**kwargs):
+        result = ""
+        # Iterating over the Python kwargs dictionary
+        for arg in kwargs.values():
+            result += arg
+        return result
+
+    print(concatenate(a="Real", b="Python", c="Is", d="Great", e="!"))
+
+
+    Returns:
+    dict: The JSON response from the server.
+
+    Raises:
+    Exception: If a network error occurs during the request.
+    """
+    params = ""
+    # Construct query string from kwargs
+    if(kwargs):
+        for key,value in kwargs.items():
+            params=params+key+"="+value+"&"
+
+    request_url = backend_url+endpoint+"?"+params
+
+    print("GET from {} ".format(request_url)) # Use flush=True for immediate output in non-interactive mode
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text

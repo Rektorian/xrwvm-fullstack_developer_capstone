3b5d9d45240112f7877ba9600c9836eeb2447966
Pawel Baczynski <pawel.baczynski86@gmail.com>
2025-07-26 15:53:30 +0200

    updates

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,10 @@
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
+// add Dealer component
+import Dealers from './components/Dealers/Dealers';
+// Add the route for /dealers to render the Dealers component.
+<Route path="/dealers" element={<Dealers/>} />
 
 function App() {
   return (
